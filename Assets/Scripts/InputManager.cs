2f2670d828566463f36cd0e8a2175ff3dcad1953
Brisc Paul <alex_brisc@yahoo.com>
2024-12-30 02:16:32 +0200

    Created FPS controller

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.PlayerActions playerActions;
+    private PlayerController controller;
+    private PlayerLook look;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        playerActions = playerInput.Player;
+
+        controller = GetComponent<PlayerController>();
+        look = GetComponent<PlayerLook>();
+
+        playerActions.Jump.performed += ctx => controller.Jump();
+
+        playerActions.Sprint.performed += ctx => controller.Sprint();
+        playerActions.Sprint.canceled += ctx => controller.Sprint();
+
+        playerActions.Crouch.performed += ctx => controller.Crouch();
+        playerActions.Crouch.canceled += ctx => controller.Crouch();
+    }
+
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        controller.ProcessMove(playerActions.Movement.ReadValue<Vector2>());
+    }
+
+    // void Update()
+    // {
+    //     controller.Sprint(playerActions.Sprint.ReadValue<float>() > 0);
+    // }
+    void LateUpdate() 
+    {
+        look.ProcessLook(playerActions.Look.ReadValue<Vector2>());
+    }
+    private void OnEnable() 
+    {
+        playerActions.Enable();
+    }
+
+    private void OnDisable() 
+    {
+        playerActions.Disable();
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+    private bool lerpCrouch;
+    private bool crounching;
+    private bool sprinting;
+    float crouchTimer = 0f;
+    [SerializeField]
+    public float playerSpeed = 0f;
+    [SerializeField]
+    public float walkSpeed = 2.5f;
+    [SerializeField]
+    public float sprintSpeed = 4f;
+    [SerializeField]
+    public float jumpHeight = 3.0f;
+    [SerializeField]
+    public float gravityValue = -9.81f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = characterController.isGrounded;
+
+        if(lerpCrouch)
+        {
+            crouchTimer += Time.deltaTime;
+            float p = crouchTimer / 1;
+            p *= p;
+            if(crounching)
+            {
+                characterController.height = Mathf.Lerp(characterController.height, 1, p);
+            }
+            else
+            {
+                characterController.height = Mathf.Lerp(characterController.height, 2, p);
+            }
+            if(p > 1)
+            {
+                lerpCrouch = false;
+                crouchTimer = 0f;
+            }
+        }
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        moveDirection = transform.TransformDirection(moveDirection) * Time.deltaTime * playerSpeed;
+        characterController.Move(moveDirection);
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        
+        characterController.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+
+    public void Jump()
+    {
+        if(isGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+    }
+
+    // public void Sprint(bool isSprinting)
+    // {
+    //     playerSpeed = isSprinting ? sprintSpeed : walkSpeed;
+    // }
+
+    public void Sprint()
+    {
+        sprinting = !sprinting;
+        playerSpeed = sprinting ? sprintSpeed : walkSpeed;
+    }
+
+    public void Crouch()
+    {
+        crounching = !crounching;
+        crouchTimer = 0f;
+        lerpCrouch = true;
+    }
+}
